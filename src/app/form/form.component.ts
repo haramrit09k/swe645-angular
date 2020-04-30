6d82838a9188cfbb9839cc54208c78dafb97e9ec
Haramrit Singh Khurana <haramrit09k@gmail.com>
2020-04-30 13:50:51 -0400

    Update form.component.ts

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,7 +7,7 @@ import { FormGroup, ReactiveFormsModule, FormBuilder, FormControl, FormArray, Va
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  SERVER_URL = "http://ec2-3-90-139-158.compute-1.amazonaws.com/jersey-quickstart-webapp/webapi/myresource/create";
+  SERVER_URL = "http://ec2-3-86-94-214.compute-1.amazonaws.com/jersey-quickstart-webapp/webapi/myresource/create";
   uploadForm: FormGroup;  
 
   constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) { 
